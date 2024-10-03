4c776260bdcebc41506518a9752feb01b0f9a1a4
chakkaravarthyJs <chakkarajs1203@gmail.com>
2024-10-03 11:02:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="APP">
+     <div className="container">
+
+     </div>
+    </div>
+  );
+}
+
+export default App;
